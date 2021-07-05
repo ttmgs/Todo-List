bcf1a3c7ecded9911d56be58d128b7708cd1f095
Magdi Tiea <magditiea@gmail.com>
2021-07-05 14:08:46 -0400

    create custom lists added

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ app.use(express.static("public"));
 // mongodb connection
 mongoose.connect("mongodb://localhost:27017/toDB", {useNewUrlParser: true})
 
-// schema
+// schemas
 const listSchema = mongoose.Schema({
   item: String
 }) 
+
+const listedSchema = ({
+  name: String,
+  items: [listSchema]
+})
+const Title = mongoose.model("Title", listedSchema)
+
 
 // model
 const List = mongoose.model("List", listSchema)
@@ -25,45 +32,104 @@ const List = mongoose.model("List", listSchema)
 const first = new List({
   item: "first Item"
 });
-
-
-
-
+const defaultItems = [first]
 
 app.get('/', (req, res) => {
-
-  List.find(function(err, lists) {
+  List.find({}, function(err, lists) {
+    if (lists.length === 0) {
+      List.insertMany(defaultItems, function(err) {
     if (err) {
       console.log(err)
     } else {
-      res.render("list", {listtitle: "Everyday is a blessing", items: lists})
-    };
+      console.log("successfully saved items to DB")
+    }
   });
-
-
-})
-
-app.post("/", (req, res) => {
-  var itemInput = req.body.input
- 
-const newlist = new List({
-  item: itemInput
-})
-newlist.save()
-res.redirect("/");
-
+      res.redirect("/");
+    } else {
+      res.render("list", {listtitle: "Today", items: lists})
+    }
+});
 });
 
-app.get("/work", (req,res) => {
-  res.render("work", {listtitle: "Work List", items: itemInputs})
+app.post("/", (req, res) => {
+  const itemInput = req.body.input
+  const listname = req.body.list
+
+  const newlist = new List({
+    item: itemInput
+  })
+  if (listname === "Today") {
+    newlist.save();
+    res.redirect("/")
+  } else {
+    Title.findOne({name: listname}, function(err, foundlist) {
+      foundlist.items.push(newlist)
+      foundlist.save();
+      res.redirect("/" + listname)
+
+    })
+
+  }
+
+
+
+  
+});
+
+app.post("/delete", (req, res) => {
+const checkedItemId = req.body.checkbox
+
+List.findByIdAndRemove(checkedItemId, function(err) {
+  if (err) {
+    console.log("error removing list item")
+  } else {
+    res.redirect("/")
+  }
 })
 
-app.post("/work", (req,res) => {
 
-  var input = req.body.winput
+})
+
+const titled = []
+
+
+
+
+app.get("/:custom", (req, res) => {
+
+  const title = req.params.custom
+
+
+
+
+Title.findOne({name: title}, function(err, foundlist) {
+  if (!err) {
+    if (!foundlist) {
+   
+// create new list
+const newItem = new Title({
+  name: title,
+  items: first
+})
+newItem.save();
+res.redirect("/" + title)
+    } else {
+// show list
+res.render("list", {listtitle: foundlist.name, items: foundlist.items})
+
+    }
+  }
+  
 
 
 })
+})
+
+
+
+
+
+
 
 
 
